28828b3d1ee928727e75dfe01c0f001631109445
Mutethiar <144267328+Mutethiar@users.noreply.github.com>
2023-10-07 21:52:46 +0300

    Add files via upload

---
+++ C functions (IF).c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+main (void)
+{
+ int b;
+printf("enter value");
+ scanf("%d",&b);
+ if(b < 0);
+ {
+     printf("the value is a negative ");
+     return 0;
+
+ }
+ return 0;
+}
